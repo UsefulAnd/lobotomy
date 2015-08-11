fd6e7d7a1bb802f560bf99f5dd1a519957cda318
Benjamin Watson <benjaminwatson@BENWAT-COTP-1.attlocal.net>
2015-08-11 12:10:12 -0400

    Adding crypto template, still a TODO

--- framework/brains/surgical/crypto.py
+++ framework/brains/surgical/crypto.py
@@ -1,1 +1,17 @@
-__author__ = 'benjaminwatson'
+from androguard.core.analysis import analysis
+
+
+class Crypto(object):
+
+    name = "crypto"
+
+    def __init__(self, apks):
+
+        super(Crypto, self).__init__()
+        self.apks = apks
+
+    def run(self):
+
+        print("crypto")
+
+        return
