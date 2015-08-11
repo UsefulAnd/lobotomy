0eec6d1fc0cda62e962fb1d9741491a013d5e980
Benjamin Watson <benjaminwatson@BENWAT-COTP-1.attlocal.net>
2015-08-11 12:08:07 -0400

    Ok the actual commit now ...

--- framework/brains/surgical/api.py
+++ framework/brains/surgical/api.py
@@ -1,18 +1,47 @@
 from framework.brains.surgical.storage import InsecureStorage
 from framework.brains.surgical.crypto import Crypto
+from datetime import datetime
+from blessings import Terminal
+t = Terminal()
 
 
 class SurgicalAPI(object):
 
-    def __init__(self, apk):
+    def __init__(self, apks):
 
-        super(self, SurgicalAPI).__init__()
-        self.apk = apk
-        self.storage = InsecureStorage()
-        self.crypto = Crypto()
+        super(SurgicalAPI, self).__init__()
+        self.apk = apks
+        self.storage = InsecureStorage(apks)
+        self.crypto = Crypto(apks)
         self.functions = [f for f in self.storage, self.crypto]
 
     def run_surgical(self):
 
-        print("testing")
+        """
+        Helper function for API
+        """
+        print(t.green("[{0}] ".format(datetime.now()) +
+                      t.yellow("Available functions: ")))
 
+        for f in self.functions:
+            print(t.green("[{0}] ".format(datetime.now())) +
+                  f.__getattribute__("name"))
+
+        print(t.green("[{0}] ".format(datetime.now()) +
+                      t.yellow("Enter \'quit\' to exit")))
+        while True:
+            # Assign target API
+            # function
+            #
+            function = raw_input(t.green("[{0}] ".format(datetime.now()) + t.yellow("Enter function: ")))
+
+            if function == "quit":
+                break
+
+            # Match on Class attribute
+            # and call run() function
+            # of target class
+            #
+            for f in self.functions:
+                if function == f.__getattribute__("name"):
+                    f.run()
--- framework/brains/surgical/storage.py
+++ framework/brains/surgical/storage.py
@@ -1,8 +1,51 @@
 from androguard.core.analysis import analysis
+from datetime import datetime
+from blessings import Terminal
+t = Terminal()
+
+
+class InternalStorageEnum(object):
+
+    values = ["getExternalFilesDir", "getSharedPreferences"]
 
 
 class InsecureStorage(object):
 
-    def __init__(self):
+    name = "storage"
 
-        super(self, InsecureStorage).__init__()
+    def __init__(self, apks):
+
+        super(InsecureStorage, self).__init__()
+        self.apks = apks
+        self.enum = InternalStorageEnum()
+
+    def run(self):
+
+        x = analysis.uVMAnalysis(self.apks.get_vm())
+        vm = self.apks.get_vm()
+
+        if x:
+            print(t.green("[{0}] ".format(datetime.now()) + t.yellow("Performing surgery ...")))
+            # Get enum values
+            #
+            for v in self.enum.values:
+                # This returns PathP
+                # objects into a list
+                #
+                paths = x.get_tainted_packages().search_methods("android.content.Context", "{0}".format(v), ".")
+
+                if paths:
+                    for p in paths:
+                        for method in self.apks.get_methods():
+                            if method.get_name() == p.get_src(vm.get_class_manager())[1]:
+                                if method.get_class_name() == p.get_src(vm.get_class_manager())[0]:
+                                    print(t.green("[{0}] ".format(datetime.now()) +
+                                          t.yellow("Found: ") +
+                                          "{0}".format(v)))
+                                    print(t.green("[{0}] ".format(datetime.now()) +
+                                                  t.yellow("Class: ") +
+                                                  "{0}".format(method.get_class_name())))
+                                    print(t.green("[{0}] ".format(datetime.now()) +
+                                                  t.yellow("Method: ") +
+                                                  "{0}".format(method.get_name())))
+                                    print(method.show())
